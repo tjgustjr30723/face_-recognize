601644d948bfc4076a36ac0823f0a58cd4362461
dlrbdjs <69340907+dlrbdjs@users.noreply.github.com>
2024-06-05 19:46:28 +0900

    PCA Ver.3 (3100장 데이터, 오류 많음)

--- real_face.py
+++ real_face.py
@@ -5,37 +5,49 @@ from sklearn.decomposition import PCA
 
 face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
 
-names = ["KRE", "KYW", "SHS", "LGE"]
-team_name = 'HMH'
+
+
+names = ["PIW", "KHH", "CSB", "KDM", "LSE", 
+         "PJS_team2", "KMG", "KMS_team2", "LJH", "KUS", 
+         "KHY", "KMJ_team3", "KKJ", "KMS_team3", 
+         "KRE", "KYW", "SHS", "LGE", 
+         "CJY", "JUH", "MJY", "PSG", 
+         "LSC", "RYJ", "LDW", "YSB", 
+         "CYW", "JHS", "KMJ_team7", "PJS_team7", "YYS"]
+
+# team_name = 'HMH'
 
 images=[]
 labels=[]
+total_recog = []
 
-
-for label, target_name in enumerate(names):
-    for i in range(1, 101):
-        default_image = cv2.imread(f'dataset/{team_name}_{target_name}/{team_name}_{target_name}_{i}.jpg')
+for i, target_name in enumerate(names):
+    recog_cnt = 0
+    for j in range(1, 101):
+        default_image = cv2.imread(f'dataset/{target_name}/{target_name}_{j}.jpg')
         gray_image = cv2.cvtColor(default_image, cv2.COLOR_BGR2GRAY)
-        face_detection = face_detector.detectMultiScale(gray_image, scaleFactor = 1.2, minNeighbors=7, minSize=(120,120))
+        face_detection = face_detector.detectMultiScale(gray_image, scaleFactor = 1.2, minNeighbors=5, minSize=(120,120))
         for (x, y, w, h) in face_detection:
+            recog_cnt += 1
             face_img = gray_image[y:y+h, x:x+w]
             face_img = cv2.resize(face_img, (200, 200))
             face_img = face_img / 255.0
             images.append(face_img.flatten())
-            labels.append(label)  # 각 사람에 대한 레이블 할당: 0, 1, 2, 3
+            labels.append(target_name)  # 각 사람에 대한 레이블 할당: 0, 1, 2, 3
             
 
         flip_default_image = cv2.flip(default_image, 1) #좌우반전
         gray_image = cv2.cvtColor(flip_default_image, cv2.COLOR_BGR2GRAY)
-        face_detection = face_detector.detectMultiScale(gray_image, scaleFactor = 1.2, minNeighbors=7, minSize=(120,120))
+        face_detection = face_detector.detectMultiScale(gray_image, scaleFactor = 1.2, minNeighbors=5, minSize=(120,120))
         for (x, y, w, h) in face_detection:
+            recog_cnt += 1
             face_img = gray_image[y:y+h, x:x+w]
             face_img = cv2.resize(face_img, (200, 200))
             face_img = face_img / 255.0
             images.append(face_img.flatten())
-            labels.append(label)  # 각 사람에 대한 레이블 할당: 0, 1, 2, 3
-            
-
+            labels.append(target_name)  # 각 사람에 대한 레이블 할당: 0, 1, 2, 3
+    total_recog.append(recog_cnt)
+    print(target_name)
             # cv2.imshow("default_image", default_image)
             # cv2.imshow("face_img", face_img)
             # cv2.waitKey(0)
@@ -43,24 +55,11 @@ for label, target_name in enumerate(names):
 images = np.array(images)
 labels = np.array(labels)
 
-# 얼굴인식률 ###################################
+# 얼굴인식확인 ###################################
 
-cnt1 = 0
-cnt2 = 0
-cnt3 = 0
-cnt4 = 0
+for i, target_name in enumerate(names):
+    print("name : %s, count : %d" %(target_name, total_recog[i]))
 
-for i in range(len(labels)):
-    if labels[i] == 0:
-        cnt1 += 1
-    elif labels[i] == 1:
-        cnt2 += 1
-    elif labels[i] == 2:
-        cnt3 += 1
-    elif labels[i] == 3:
-        cnt4 += 1
-
-print(cnt1, cnt2, cnt3, cnt4)
 ##############################################
 
 pca = PCA(n_components=0.95)  # 데이터의 95% 분산을 설명하는 주성분 개수 선택
@@ -73,7 +72,7 @@ eigenfaces = pca.components_  # 주성분 벡터 (Eigenfaces)
 def predict(img):
     main_img = img
     new_face = cv2.cvtColor(main_img, cv2.COLOR_BGR2GRAY)
-    test_face_detection = face_detector.detectMultiScale(new_face, scaleFactor = 1.2, minNeighbors=4, minSize=(120,120))
+    test_face_detection = face_detector.detectMultiScale(new_face, scaleFactor = 1.2, minNeighbors=5, minSize=(120,120))
 
     for (x, y, w, h) in test_face_detection:
         test_face = new_face[y:y+h, x:x+w]
@@ -94,6 +93,7 @@ def predict(img):
 
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         cv2.putText(img, f'Label: {closest_label}', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        cv2.putText(img, f'Distances: {distances}', (x, y+h+10), cv2.FONT_HERSHEY_SIMPLEX, 0.1, (255, 0, 0), 2)
         # cv2.putText(color_image, f'Accuracy: {np.max(prediction) * 100:.2f}%', (x, y + h + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
         cv2.imshow('Face Recognition', img)
 
